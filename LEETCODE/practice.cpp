4ed1567a4059856dc03ee8456716059c9c5005c0
saurabhkumar70049 <saurabhbarej@gmail.com>
2023-01-14 23:42:14 +0530

    new questio

--- LEETCODE/practice.cpp
+++ LEETCODE/practice.cpp
@@ -1,33 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void getMaximumXor(vector<int>& nums, int maximumBit) {
-        vector<int> num2(nums.size());
-        
-        for (int i = 0; i < nums.size(); i++) {
-            num2[i] = pow(2, maximumBit) -1;
-            for (int j = 0; j < nums.size() - i; j++) {
-                num2[i] ^= nums[j];
+vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> index(2);
+        sort(nums.begin(), nums.end());
+        int i = 0;
+        int j = nums.size()-1;
+        while(i < j) {
+            if ((nums[i]+nums[j]) == target) {
+                index[0] = i;
+                index[1] = j;
+                return index;
+            }
+            else if((nums[i]+nums[j]) > target) {
+                j--;
+            }
+            else {
+                i++;
             }
         }
-        for (auto i = num2.begin(); i != num2.end(); i++) {
-            cout << *i << " ";
-        }
+        return index;
+
     }
 int main() {
     
     vector<int> num;
-    num.reserve(4);
     int a;
     cout << "enter num : ";
     for (int i = 0; i < 4; i++) {
         cin >> a;
         num.push_back(a);
     }
-    cout << num.size();
     int max_bit;
     cin >> max_bit;
-    getMaximumXor(num, max_bit);
+    twoSum(num, max_bit);
 
     
     
