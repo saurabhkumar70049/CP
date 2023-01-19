4e6888e214627d525b313936e6f4c4d37013f572
saurabhkumar70049 <saurabhbarej@gmail.com>
2023-01-19 15:36:59 +0530

    new question

--- LEETCODE/practice.cpp
+++ LEETCODE/practice.cpp
@@ -1,29 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int findKthPositive(vector<int>& arr, int k) {
-        int n = arr.size();
-        int i = 0, j = n-1;
-        while(i <= j) {
-            int mid = (i+j)/2;
-            cout << mid << " " << arr[mid] << endl;
-            if (((arr[mid] - (mid+1)) < k) && ((mid == n-1) || (arr[mid+1] - (mid+2)) >= k)) {
-                return (k + mid + 1);
-            }
-            else if((arr[mid] - (mid+1)) >= k){
-                j = mid - 1;
-            }
-            else {
-                i = mid + 1;
-            }
+void targetIndices(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); i++) {
+            cout << nums[i] << " ";
         }
-        return k;
-
+        int k = upper_bound(nums.begin(), nums.end(), target) - nums.begin();
+        int j = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+        cout << endl;
+        cout << j << " " << k ;
+        if (nums[k-1] == target && nums[j] == target) {
+            cout << "yes";
+        }
         
     }
 int main() {
     
-    vector<int> num = {1,4,7,9,14,15,16,18,20,23,24,25,26,28,29,30,33,35,38,39,42,44,46,48,49,52,53,55,57,58,60,63,68,69,70};
+    vector<int> num = {1,2,5,2,3};
     // int a;
     // cout << "enter num : ";
     // for (int i = 0; i < 1; i++) {
@@ -32,7 +26,7 @@ int main() {
     // }
     int max_bit;
     cin >> max_bit;
-    cout << findKthPositive(num, max_bit);
+    targetIndices(num, max_bit);
 
     
     
