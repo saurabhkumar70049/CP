ce09becec093b122d28a8c9162ff436a497d2830
saurabhkumar70049 <saurabhbarej@gmail.com>
2023-02-14 00:23:10 +0530

    done

--- LEETCODE/practice.cpp
+++ LEETCODE/practice.cpp
@@ -1,41 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> sortArrayByParityII(vector<int>& arr) {
-        int n = arr.size();
-        int l = 0, r = n-1;
-        while(l < r) {
-            while(((l%2 == 0 && arr[l]%2 == 0) || (l%2 != 0 && arr[l]%2 != 0))) {
-                l++;
-                cout << l << " ";
-                if(l >=r) {
-                    break;
-                }
+void checkArithmeticSubarrays(vector<int>& nums, vector<int>& l, vector<int>& r) {
+        int n = l.size();
+        vector<bool> vec1; 
+        for(int i = 0; i < n; i++) {
+            vector<int> vec2(nums.begin()+l[i], nums.begin()+r[i]+1);
+            for(int i : vec2) {
+                cout << i << " ";
             }
             cout << endl;
-            while(((r%2 == 0 && arr[r]%2 == 0) || (r%2 != 0 && arr[r]%2 != 0)) ) {
-                r--;
-                cout << r << " ";
-                if(l >=r) {
-                    break;
+            sort(vec2.begin(), vec2.end());
+            bool res = true;
+            int d = vec2[1]-vec2[0];
+            for(int j = 2; j < vec2.size(); j++) {
+                if((vec2[j]-vec2[j-1]) != d) {
+                    res = false;
                 }
             }
-            cout << endl;
-            if(l >=r) {
-                break;
-            }
-            swap(arr[l], arr[r]);
-            r--;
-            l++;
-            
+            vec1.push_back(res);
         }
-        return arr;
+        for(auto i : vec1) {
+            i == 0 ? cout << "flase" << " " : cout << "true" <<" ";
+        }
     }
 
+
 int main() {
-    vector<int> vec = {2,3,3,1,4,4,0,0,1,3};
-    vector<int> vec1 = sortArrayByParityII(vec);
-    for(int i : vec1) {
-        cout << i << " ";
-    }
+    vector<int> vec = {4,6,5,9,3,7};
+    vector<int> l = {0,0,2};
+    vector<int> r = {2,3,5};
+    checkArithmeticSubarrays(vec, l, r);
 }
